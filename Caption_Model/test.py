dd3136dd0c10f0e8b2b440d89e430e15828e102f
sithara <sithara.kamalakkannan@gmail.com>
2019-12-05 21:29:50 -0800

    uploading code for captioning module

---
+++ Caption_Model/test.py
@@ -0,0 +1,60 @@
+import tensorflow as tf
+import pickle
+from model import Model
+from utils import build_dict, build_dataset, batch_iter
+import os
+
+with open("args.pickle", "rb") as f:
+    args = pickle.load(f)
+
+print("Loading dictionary...")
+word_dict, reversed_dict, article_max_len, summary_max_len = build_dict("valid", args.toy)
+print("Loading validation dataset...")
+valid_x = build_dataset("valid", word_dict, article_max_len, summary_max_len, args.toy)
+valid_x_len = [len([y for y in x if y != 0]) for x in valid_x]
+
+if os.path.exists('/home/rupsha_8/text-summarization-tensorflow/result.txt'):
+    os.remove('/home/rupsha_8/text-summarization-tensorflow/result.txt')
+with tf.Session() as sess:
+    print("Loading saved model...")
+    model = Model(reversed_dict, article_max_len, summary_max_len, args, forward_only=True)
+    saver = tf.train.Saver(tf.global_variables())
+    ckpt = tf.train.get_checkpoint_state("./saved_model/")
+    saver.restore(sess, ckpt.model_checkpoint_path)
+
+    batches = batch_iter(valid_x, [0] * len(valid_x), args.batch_size, 1)
+
+    print("Writing summaries to 'result.txt'...")
+    for batch_x, _ in batches:
+        batch_x_len = [len([y for y in x if y != 0]) for x in batch_x]
+
+        valid_feed_dict = {
+            model.batch_size: len(batch_x),
+            model.X: batch_x,
+            model.X_len: batch_x_len,
+        }
+
+        prediction = sess.run(model.prediction, feed_dict=valid_feed_dict)
+        prediction_output = [[reversed_dict[y] for y in x] for x in prediction[:, 0, :]]
+
+        with open("unfiltered_result.txt", "a") as f:
+            for line in prediction_output:
+                summary = list()
+                for word in line:
+                    if word == "</s>":
+                        break
+                    
+                    summary.append(word)
+                print(" ".join(summary), file=f)
+
+        with open("result.txt", "a") as f:
+            for line in prediction_output:
+                summary = list()
+                for word in line:
+                    if word == "</s>":
+                        break
+                    if word not in summary:
+                        summary.append(word)
+                print(" ".join(summary), file=f)
+
+    print('Summaries are saved to "result.txt"...')
